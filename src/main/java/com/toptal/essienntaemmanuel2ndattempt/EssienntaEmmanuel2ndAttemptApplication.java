780bef0df1fae14e0e4ddb605b3a3151b7719440
Essiennta Emmanuel <colourfulemmanuel@gmail.com>
2017-10-14 09:02:41 +0100

    Generate project using start.spring.io
    Technology stack:
    * Spring's web application support.
    * Spring's application security support.
    * JPA for sequel based database access.
    * H2 embedded database for rapid prototyping.
    * MySQL for fine-grained inspection and reliability.

---
+++ src/main/java/com/toptal/essienntaemmanuel2ndattempt/EssienntaEmmanuel2ndAttemptApplication.java
@@ -0,0 +1,12 @@
+package com.toptal.essienntaemmanuel2ndattempt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssienntaEmmanuel2ndAttemptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EssienntaEmmanuel2ndAttemptApplication.class, args);
+	}
+}
---
+++ src/test/java/com/toptal/essienntaemmanuel2ndattempt/EssienntaEmmanuel2ndAttemptApplicationTests.java
@@ -0,0 +1,16 @@
+package com.toptal.essienntaemmanuel2ndattempt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EssienntaEmmanuel2ndAttemptApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
