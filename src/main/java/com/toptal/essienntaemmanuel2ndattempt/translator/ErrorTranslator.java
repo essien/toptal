d69fee8e5ea76b53a7dd5fda6fc0922aac53a51e
Essiennta Emmanuel <colourfulemmanuel@gmail.com>
2017-10-15 08:31:24 +0100

    Friendly presentation of AccessDeniedException

--- src/main/java/com/toptal/essienntaemmanuel2ndattempt/translator/ErrorTranslator.java
+++ src/main/java/com/toptal/essienntaemmanuel2ndattempt/translator/ErrorTranslator.java
@@ -4,6 +4,7 @@ import com.toptal.essienntaemmanuel2ndattempt.exception.GenericException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,5 +24,13 @@ public class ErrorTranslator {
     public ResponseDto processGenericException(GenericException e) {
         return new ResponseDto().setStatus(ResponseDto.FAIL)
                 .setMessage(e.getClientMessage());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ResponseBody
+    public ResponseDto processAccessDeniedException(AccessDeniedException e) {
+        return new ResponseDto().setStatus(ResponseDto.FAIL)
+                .setMessage(e.getMessage());
     }
 }
